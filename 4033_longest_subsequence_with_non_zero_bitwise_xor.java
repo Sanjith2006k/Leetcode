ee164111e8548e1252d368c9e59eb5c6ee6bb7bf
S Sanjith Kumar <ssanjithkumar2006@gmail.com>
2026-08-15 20:31:37 +0530

    Update 4033_longest_subsequence_with_non_zero_bitwise_xor.java - LeetCode Problem 4033

--- 4033_longest_subsequence_with_non_zero_bitwise_xor.java
+++ 4033_longest_subsequence_with_non_zero_bitwise_xor.java
@@ -1,23 +1,17 @@
 class Solution {
     public int longestSubsequence(int[] nums) {
         int xor = 0;
-        boolean hasNonZero = false;
-
-        for (int num : nums) {
-            xor ^= num;
-
-            if (num != 0) {
-                hasNonZero = true;
-            }
+        for(int n:nums){
+            xor ^=n;
         }
-
-        if (xor != 0) {
-            return nums.length;
+       if(xor !=0){
+        return nums.length;
+       }
+       for(int i :nums){
+        if(i != 0){
+            return nums.length -1 ;
         }
-        if (hasNonZero) {
-            return nums.length - 1;
-        }
-
-        return 0;
+       }
+    return 0;    
     }
 }
