74dda06384be1742a5c5ee2ef03e4335973c8c2d
S Sanjith Kumar <ssanjithkumar2006@gmail.com>
2026-09-03 22:51:50 +0530

    Add 415_add_strings.java - LeetCode Problem 415

---
+++ 415_add_strings.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int a = Integer.parseInt(num1);
+        int b = Integer.parseInt(num2);
+        int c= a+b;
+        String d = ""+c;
+        return d;
+        
+    }
+}
