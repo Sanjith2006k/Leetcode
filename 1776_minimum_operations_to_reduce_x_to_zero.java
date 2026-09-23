9cb0e9d9eb90099e12333feecf1b5dd6097400d0
S Sanjith Kumar <ssanjithkumar2006@gmail.com>
2026-09-23 23:17:12 +0530

    Add 1776_minimum_operations_to_reduce_x_to_zero.java - LeetCode Problem 1776

---
+++ 1776_minimum_operations_to_reduce_x_to_zero.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int total = 0;
+
+        for (int i=0;i<nums.length;i++) {
+            total += nums[i];
+        }
+
+        int target = total - x;
+
+        if (target < 0) {
+            return -1;
+        }
+
+        if (target == 0) {
+            return nums.length;
+        }
+
+        int left = 0;
+        int sum = 0;
+        int maxLen = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            while (sum > target && left <= i) {
+                sum -= nums[left++];
+            }
+
+            if (sum == target) {
+                maxLen = Math.max(maxLen, i - left + 1);
+            }
+        }
+
+        if(maxLen == -1)
+        return -1;
+        return -(maxLen-nums.length); 
+    }
+}
